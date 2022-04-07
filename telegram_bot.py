a392b1454e4bee7cf1f0ca693fa47311c9343a1f
Pablo Pazo Jiménez <43904563+pabpazjim@users.noreply.github.com>
2022-04-07 14:01:37 +0200

    telegram bot script clean

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,6 +11,3 @@ def send_telegram_notification(notification):
 
     bot.send_message(text=notification, chat_id=chat_id,
                      parse_mode='Markdown', disable_web_page_preview=True)
-
-
-print(telegram.ParseMode)
