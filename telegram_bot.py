4b93b51f0c45959f4f81a7338f6ce6bb3a87e758
Pablo Pazo Jiménez <43904563+pabpazjim@users.noreply.github.com>
2022-04-07 13:21:33 +0200

    telegram bot integration

---
+++ telegram_bot.py
@@ -0,0 +1,16 @@
+import telegram
+import os
+
+token = os.environ['TELEGRAM_BOT_TOKEN']
+chat_id = os.environ['TELEGRAM_BOT_CHAT_ID']
+
+bot = telegram.Bot(token=token)
+
+
+def send_telegram_notification(notification):
+
+    bot.send_message(text=notification, chat_id=chat_id,
+                     parse_mode='Markdown', disable_web_page_preview=True)
+
+
+print(telegram.ParseMode)
