e58f0ba0a95d90ece424de1f96599c9e00e08fd7
Pablo Pazo Jiménez <43904563+pabpazjim@users.noreply.github.com>
2022-04-07 13:44:42 +0200

    added webhook secret verification

--- webhook.py
+++ webhook.py
@@ -3,12 +3,29 @@ from flask import request
 from flask import json
 from request_handler import handle_request
 from telegram_bot import send_telegram_notification
+from hmac import HMAC, compare_digest
+from hashlib import sha256
+
+import os
+
 app = Flask(__name__)
+
+webhook_secret = os.environ['WEBHOOK_SECRET']
+
+
+def verify_signature(req):
+    received_sign = req.headers.get(
+        'X-Hub-Signature-256').split('sha256=')[-1].strip()
+    secret = webhook_secret.encode()
+    expected_sign = HMAC(key=secret, msg=req.data,
+                         digestmod=sha256).hexdigest()
+    return compare_digest(received_sign, expected_sign)
 
 
 @app.route('/', methods=['POST'])
 def hook_root():
     if request.headers['CONTENT_TYPE'] == 'application/json':
+        assert(verify_signature(request))
         event = request.headers.get('X-GitHub-Event')
         json = request.get_json()
         notification = handle_request(event, json)
